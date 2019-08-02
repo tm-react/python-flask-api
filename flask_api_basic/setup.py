bb48469f48e76552a6ca44fe2be4287c15998583
Touhid Mia <hmtm.cse@gmail.com>
2019-08-03 00:28:35 +0600

    Flask API Basic

--- flask_api_basic/setup.py
+++ flask_api_basic/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flask_api',
+    name='flask_api_basic',
     version='1.0.0',
     description='Boilerplate code for a Python Flask RESTful API',
     url='https://github.com/tm-react/python-flask-api',
